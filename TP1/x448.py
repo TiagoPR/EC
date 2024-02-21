60fe7da671dc0e1d3ea8a98c7889a26ae184148c
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2024-02-21 17:44:19 +0000

    My friend code

--- TP1/x448.py
+++ TP1/x448.py
@@ -1,34 +1,45 @@
-from cryptography.hazmat.primitives import hashes
+import os
 from cryptography.hazmat.primitives.asymmetric.x448 import X448PrivateKey
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-from cryptography.hazmat.primitives.ciphers.algorithms import AES256
+from cryptography.hazmat.primitives.asymmetric.ed448 import Ed448PrivateKey
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
-# Generate a private key for use in the exchange.
-private_key = X448PrivateKey.generate()
+# Geração das chaves privadas X448 e Ed448
+x448_private_key = X448PrivateKey.generate()
+ed448_private_key = Ed448PrivateKey.generate()
 
-# In a real handshake the peer_public_key will be received from the
-# other party. For this example we'll generate another private key and
-# get a public key from that. Note that in a DH handshake both peers
-# must agree on a common set of parameters.
+# Geração das chaves públicas X448 e Ed448
+x448_public_key = x448_private_key.public_key()
+ed448_public_key = ed448_private_key.public_key()
 
-peer_public_key = X448PrivateKey.generate().public_key()
-shared_key = private_key.exchange(peer_public_key)
+# Acordo de chaves com X448
+shared_key = x448_private_key.exchange(x448_public_key)
 
-# Perform key derivation.
-derived_key = HKDF(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=None,
-    info=b'handshake data',
-).derive(shared_key)
+# Assinatura da chave compartilhada com Ed448
+signature = ed448_private_key.sign(shared_key)
 
-# For the next handshake we MUST generate another private key.
-private_key_2 = X448PrivateKey.generate()
-peer_public_key_2 = X448PrivateKey.generate().public_key()
-shared_key_2 = private_key_2.exchange(peer_public_key_2)
-derived_key_2 = HKDF(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=None,
-    info=b'handshake data',
-).derive(shared_key_2)
+# Verificação da assinatura com Ed448
+try:
+    ed448_public_key.verify(signature, shared_key)
+    print("A assinatura é válida.")
+except:
+    print("A assinatura é inválida.")
+
+# Criação do canal privado de informação com ChaCha20Poly1305
+chacha = ChaCha20Poly1305(shared_key)
+
+# Dados a serem criptografados
+data = b"mensagem secreta"
+
+# Dados associados
+aad = b"dados associados"
+
+# Criptografar os dados
+nonce = os.urandom(12)  # 96 bits
+ct = chacha.encrypt(nonce, data, aad)
+
+# Descriptografar os dados
+pt = chacha.decrypt(nonce, ct, aad)
+
+print("Texto plano:", pt)
