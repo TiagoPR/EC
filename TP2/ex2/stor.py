7ef951953a2d5c03549396b3afcebba9f58647c0
Tiago Rodrigues <tiagopassosrodrigues@hotmail.com>
2024-03-30 16:33:07 +0000

    Comment ex2

--- TP2/ex2/stor.py
+++ TP2/ex2/stor.py
@@ -1,32 +1,7 @@
 from sage.all import *
 
 class NTT(object):
-    """
-        Class representing the Number Theoretic Transform (NTT).
-
-        The NTT class provides methods for performing the forward and inverse NTT on polynomials.
-
-        Args:
-            n (int): The size of the polynomial. Must be one of [32, 64, 128, 256, 512, 1024, 2048].
-            q (int, optional): The modulus value. If not provided, a suitable value of q is chosen based on the rules of NTT.
-
-        Raises:
-            ValueError: If the value of n is not one of the allowed sizes.
-
-        Attributes:
-            n (int): The size of the polynomial.
-            q (int): The modulus value.
-            F (FiniteField): The finite field used for computations.
-            R (PolynomialRing): The polynomial ring used for computations.
-            xi (Element): The primitive root of unity.
-            base (list): The Chinese Remainder Theorem (CRT) basis.
-
-        Methods:
-            ntt(f): Performs the forward NTT on the input polynomial f.
-            ntt_inv(ff): Performs the inverse NTT on the input polynomial ff.
-            random_pol(args): Generates a random polynomial.
-
-        """
+    # Construtor
     # O primeiro passo é a escolha de um $N$ da forma $2^d$  e um primo $\,q\,$ que  verifique $\,q \equiv 1 \bmod 2N\,$.
     def __init__(self, n=128, q=None):
         if not  n in [32,64,128,256,512,1024,2048]:
@@ -46,37 +21,39 @@ class NTT(object):
                 raise ValueError("Valor de 'q' não verifica a condição NTT")
             self.q = q
             
-        ##########
-            
         # Define o campo finito e o anel de polinómios
         self.F = GF(self.q) ;  self.R = PolynomialRing(self.F, name="w")
-        w = (self.R).gen()
-        
+        w = (self.R).gen() # variável w do anel de polinómios R
+
         # Calcula a raiz primitiva da unidade xi
         g = (w**n + 1) # 
-        xi = g.roots(multiplicities=False)[-1] # 
+        xi = g.roots(multiplicities=False)[-1] # obtemos raíz primitiva da unidade xi
         self.xi = xi
-        rs = [xi**(2*i+1)  for i in range(n)] 
-        self.base = crt_basis([(w - r) for r in rs])  
-    
+        raizes = [xi**(2*i+1)  for i in range(n)] # obtemos as raízes de unidade xi^(2i+1)
+        self.base = crt_basis([(w - raiz) for raiz in raizes]) # construção da base do teorema chinês do resto
+        print(self.base[1])
+        # E = crt_basis(X)
+        # X - lista de inteiros que são coprimos em pares
+        # E - lista de inteiros de tal modo que E[i] = 1 (mod X[i]) e E[i] = 0 (mod X[j]), sendo que j != i
     
     def ntt(self,f):
-        def _expand_(f): 
-            u = f.list()
-            return u + [0]*(self.n-len(u)) 
+        def _expand_(f):
+            u = f.list() # lista dos coeficientes do polinómio f
+            return u + [0]*(self.n-len(u)) # expande o polinómio f para o tamanho n
         
         def _ntt_(xi,N,f):
             if N==1:
                 return f
-            N_ = N//2 ; xi2 =  xi**2  
-            f0 = [f[2*i]   for i in range(N_)] ; f1 = [f[2*i+1] for i in range(N_)] 
-            ff0 = _ntt_(xi2,N_,f0) ; ff1 = _ntt_(xi2,N_,f1)  
+            N_ = N//2 ; #  N / 2 coeficientes 
+            xi2 =  xi**2 # xi^2
+            f0 = [f[2*i]   for i in range(N_)] ; f1 = [f[2*i+1] for i in range(N_)] # divide f em f0 par e f1 ímpar (split)
+            ff0 = _ntt_(xi2,N_,f0) ; ff1 = _ntt_(xi2,N_,f1) # recursão
     
-            s  = xi ; ff = [self.F(0) for i in range(N)] 
+            s  = xi ; ff = [self.F(0) for _ in range(N)] # inicializa ff (transformada) com zeros (polinómio de tamanho N)
             for i in range(N_):
                 a = ff0[i] ; b = s*ff1[i]  
-                ff[i] = a + b ; ff[i + N_] = a - b 
-                s = s * xi2                     
+                ff[i] = a + b ; ff[i + N_] = a - b # calcula ff[i] e ff[i + N/2]
+                s = s * xi2 # atualiza s
             return ff 
         
         return _ntt_(self.xi,self.n,_expand_(f))
@@ -92,11 +69,14 @@ class NTT(object):
 #T = NTT(n=1024)
 T = NTT(n=2048,q=343576577)
 
+# Temos o polinómio f
 f = T.random_pol(1023)
 
+# Aplicamos a transformada NTT a f
 ff = T.ntt(f)
 
+# Obtemos o polinómio f que é a transformada inversa de ff
 fff = T.ntt_inv(ff)
 
-# print(fff)
+# Verificamos se f e fff são iguais
 print("Correto ? ",f == fff)
